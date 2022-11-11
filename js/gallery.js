dde56c33d23d618d3933a419043086ed761969f3
Dmitriy-Kolomiec <95557569+Dmitriy-Kolomiec@users.noreply.github.com>
2022-11-11 23:39:40 +0800

    all edit

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,12 @@
-const bigImg = document.querySelectorAll('.bigimg');
-function setBigImage(foto) {
-    
-    bigImg.src = foto.src;
-  }
+const imageVariants = document.querySelectorAll('.js-photo-work-preview');
+
+for (let i = 0; i < imageVariants.length; i++) {
+    const imageVariant = imageVariants[i];
+
+    imageVariant.addEventListener('click', (e) => {
+        const imageVariantSrc = e.target.src;
+        const imagePreview = e.target.closest('.js-photo-work').querySelector('.js-preview');
+
+        imagePreview.setAttribute('src', imageVariantSrc);
+    });
+}
