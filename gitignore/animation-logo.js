2a6377d0efd59690283f0613b98e52d4a150d067
Dmitriy-Kolomiec <95557569+Dmitriy-Kolomiec@users.noreply.github.com>
2022-12-02 21:52:56 +0800

    add ignore

---
+++ gitignore/animation-logo.js
@@ -0,0 +1,28 @@
+// Обучение, анимация с помощью JS
+
+// const logo = document.querySelector('.logo-js')
+
+// let isPaused = false; // остановка
+// let degrees = 0; // Текущие количество градусов, на которое прокрутился элемент
+
+// function animate() {
+//     if (!isPaused) {
+//         degrees++;
+//         logo.style.transform = `rotate(${degrees}deg)`;
+
+//         requestAnimationFrame(animate);
+//     }
+// };
+
+// animate();
+
+// logo.addEventListener('mouseenter', function() {
+//     isPaused = true;
+// });
+// logo.addEventListener('mouseleave', function() {
+//     isPaused = false;
+//     animate();
+// });  
+
+
+
