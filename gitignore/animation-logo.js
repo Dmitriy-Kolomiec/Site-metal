247c6b303f0ffc73e04a3d335226c0fa2321f033
Dmitriy-Kolomiec <95557569+Dmitriy-Kolomiec@users.noreply.github.com>
2022-12-11 17:47:17 +0800

    a small revision

--- gitignore/animation-logo.js
+++ gitignore/animation-logo.js
@@ -1,29 +1,48 @@
-// Обучение, анимация с помощью JS
+// // Обучение, анимация с помощью JS
 
-const logo = document.querySelector('.logo-js')
+// const logo = document.querySelector('.logo-js')
 
-let isPaused = false; // остановка
-let degrees = 0; // Текущие количество градусов, на которое прокрутился элемент
+// let isPaused = false; // остановка
+// let degrees = 0; // Текущие количество градусов, на которое прокрутился элемент
 
-function animate() {
-    if (!isPaused) {
-        degrees++;
-        logo.style.transform = `rotate(${degrees}deg)`;
+// function animate() {
+//     if (!isPaused) {
+//         degrees++;
+//         logo.style.transform = `rotate(${degrees}deg)`;
 
-        requestAnimationFrame(animate);
+//         requestAnimationFrame(animate);
+//     }
+// };
+
+// animate();
+
+// logo.addEventListener('mouseenter', function() {
+//     isPaused = true;
+// });
+// logo.addEventListener('mouseleave', function() {
+//     isPaused = false;
+//     animate();
+// }); 
+
+// WEB Animations API
+
+const logo = document.querySelector('.logo-js');
+
+const animation = logo.animate(
+    [
+        {transform: 'translateX(0)'}, // Начальное положение
+        {transform: 'translate(70px)'},
+        {transform: 'translateX(0)'}  // финишное положение
+    ],
+    {
+    duration: 5000, // количество милисекунд на итерацию
+    iterations: Infinity // количество итераций.
     }
-};
-
-animate();
+);
 
 logo.addEventListener('mouseenter', function() {
-    isPaused = true;
+    animation.pause();
 });
 logo.addEventListener('mouseleave', function() {
-    isPaused = false;
-    animate();
-});  
-
-//проверка
-
-
+    animation.play();
+});
--- js/maps.js
+++ js/maps.js
@@ -1,4 +1,3 @@
-const country = [46.14928254793879,42.268274983252056];
 const office = [45.0158453190553,39.06788968650813];
 const production =[44.97954386943383,38.94785041268918];
 
@@ -7,12 +6,9 @@ function unit() {
         center: office,
         zoom: 17
     });
-    let placemark = new ymaps.Placemark(office, {}, { /* Приваеваем метку*/
+    let placemark = new ymaps.Placemark(office, {}, { /* Приcваеваем метку*/
 });
-    let map = new ymaps.Map('map-country', { /* подгружаем карту юга страны*/
-        center: country,
-        zoom: 6
-    });
+
 map.controls.remove('searchControl');
 map.controls.remove('trafficControl');
 map.geoObjects.add(placemark);
